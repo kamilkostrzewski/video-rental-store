af1e3e7bf6aef542109ab9e1df8d9a28e7bece99
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-01-05 12:25:54 +0100

    New Movies Controller with new Random View

--- VideoRentalStore/Controllers/MoviesController.cs
+++ VideoRentalStore/Controllers/MoviesController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoRentalStore.Models;
 
 namespace VideoRentalStore.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Random()
         {
-            return View();
+            var movie = new Movie(){ Name = "Shrek!"};
+            return View(movie);
         }
+
+         
     }
 }
