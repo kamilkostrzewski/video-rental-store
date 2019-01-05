e29d4c6e8ed70592cc069b05eeb17cbf1aa10221
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-01-05 16:05:06 +0100

    New Model Customer, new ViewModel RandomMovieViewModel and extra methods in controller

--- VideoRentalStore/Controllers/MoviesController.cs
+++ VideoRentalStore/Controllers/MoviesController.cs
@@ -4,18 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VideoRentalStore.Models;
+using VideoRentalStore.ViewModels;
 
 namespace VideoRentalStore.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie(){ Name = "Shrek!"};
-            return View(movie);
-        }
+            var customers = new List<Customer>()
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-         
+            return View(viewModel);
+        }
+        
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
     }
 }
