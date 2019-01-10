e8fbe19682ee48ed847d46913728cbf69ad62bb4
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-01-10 22:55:14 +0100

    Added Gengres table MIGRATION

---
+++ VideoRentalStore/Migrations/201901102115391_NewGenresTable.cs
@@ -0,0 +1,26 @@
+namespace VideoRentalStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Genres");
+        }
+    }
+}
