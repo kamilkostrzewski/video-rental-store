2f44ffad67171ec76cc0fe2ce72d95b283b65386
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-01-10 22:57:31 +0100

    Modified Movie Model

--- VideoRentalStore/Models/Movie.cs
+++ VideoRentalStore/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,16 @@ namespace VideoRentalStore.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public int NumberInStock { get; set; }
     }
 }
