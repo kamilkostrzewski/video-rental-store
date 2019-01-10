985fb42e4714e302b8f2c63a91d58ea66a70dc5c
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-01-10 20:57:49 +0100

    Added Name property to MembershipType model

--- VideoRentalStore/Models/MembershipType.cs
+++ VideoRentalStore/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace VideoRentalStore.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
         public int SignUpFee { get; set; }
+
         public byte DurationInMonths { get; set; }
+
         public byte DiscountRate { get; set; }
     }
 }
