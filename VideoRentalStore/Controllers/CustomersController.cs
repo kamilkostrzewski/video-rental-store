5887b8e05b58ead3872789155e399b5ea2f314c4
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-01-10 21:27:14 +0100

    Added displaying Birthdate to Customers/Details view

--- VideoRentalStore/Controllers/CustomersController.cs
+++ VideoRentalStore/Controllers/CustomersController.cs
@@ -24,13 +24,16 @@ namespace VideoRentalStore.Controllers
 
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            var customers = _context.Customers
+                .Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customers = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customers = _context.Customers
+                .Include(c => c.MembershipType)
+                .SingleOrDefault(c => c.Id == id);
 
             if (customers == null)
             {
