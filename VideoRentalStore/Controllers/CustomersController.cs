9d6f51cb8fd30e445636e4f6d367b623129239a8
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-28 17:02:08 +0200

    Rename model and view, Add new Edit action

--- VideoRentalStore/Controllers/CustomersController.cs
+++ VideoRentalStore/Controllers/CustomersController.cs
@@ -22,17 +22,29 @@ namespace VideoRentalStore.Controllers
 
         public ActionResult New()
         {
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = _context.MembershipTypes.ToList()
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers
+                    .Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
@@ -57,6 +69,22 @@ namespace VideoRentalStore.Controllers
             }
 
             return View(customers);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
