6880f76052b746e1db32abadce506148c8950b22
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-28 17:39:02 +0200

    Add model file to AddGenreIdToMovieModel migration

--- VideoRentalStore/Models/Movie.cs
+++ VideoRentalStore/Models/Movie.cs
@@ -15,10 +15,15 @@ namespace VideoRentalStore.Models
 
         public Genre Genre { get; set; }
 
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
     }
 }
