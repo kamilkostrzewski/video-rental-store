011fcd7dbd04ff305e2911b77bed4e1884f034c7
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-28 17:37:19 +0200

    Add MovieFormViewModel and New.cshtml

---
+++ VideoRentalStore/ViewModel/MovieFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoRentalStore.Models;
+
+namespace VideoRentalStore.ViewModel
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
